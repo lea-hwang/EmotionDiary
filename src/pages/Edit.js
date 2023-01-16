a14d8f44b695253cf110441257a1b1368acbc372
heewon Hwang <huiwon1488@gmail.com>
2023-01-16 17:24:38 +0900

    [FEATURE] setting router

---
+++ src/pages/Edit.js
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
+// Edit.js
+const Edit = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Edit</h1>
+      <p>이곳은 일기 수정 페이지입니다.</p>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home으로 이동
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
+    </div>
+  );
+};
+
+export default Edit;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+// Home.js
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>이곳은 Home 입니다.</p>
+    </div>
+  );
+};
+
+export default Home;
