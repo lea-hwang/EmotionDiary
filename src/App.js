412a4b9b32dc401861841f8d6675fde8996097ed
heewon Hwang <huiwon1488@gmail.com>
2023-01-17 13:28:03 +0900

    [SETTING] project setting - font, image, base component, layout

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
-import RouteTest from "./components/RouteTest";
+
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
         <h2>App.js</h2>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,7 +32,6 @@ function App() {
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
